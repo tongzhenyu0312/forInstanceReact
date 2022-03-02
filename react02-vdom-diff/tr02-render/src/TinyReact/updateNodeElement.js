17f11c2301d1fd903a5486c52351c4cb7944a985
tongzhenyu <742502346@qq.com>
2022-03-02 17:29:00 +0800

    fix event bind target

--- react02-vdom-diff/tr02-render/src/TinyReact/updateNodeElement.js
+++ react02-vdom-diff/tr02-render/src/TinyReact/updateNodeElement.js
@@ -5,7 +5,7 @@ export default function updateNodeElement (newElement, vdom) {
     if (propName.slice(0,2) === 'on') {
       // 事件属性(onClick...)
       const eventName = propName.toLowerCase().slice(2);
-      document.addEventListener(eventName, propValue);
+      newElement.addEventListener(eventName, propValue);
     } else if (propName === 'value' || propName === 'checked') {
       newElement[propName] = propValue;
     } else if (propName !== 'children') {
